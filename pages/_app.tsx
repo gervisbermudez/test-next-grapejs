3dbeb6c545fc28fdb3dab20d81e00c8599e10acd
Gervis Bermudez <gervisbermudez15@gmail.com>
2022-05-18 17:55:45 -0300

    init editor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import 'grapesjs/dist/css/grapes.min.css';
+import '../styles/editor.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
